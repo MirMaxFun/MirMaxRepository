f72cc307361b527511109ded80dc16508519c83d
MirMaxFun <wayday228@gmail.com>
2023-03-11 20:41:10 +0400

    HomeWork3

--- MirMaxFun/src/Programm.java
+++ MirMaxFun/src/Programm.java
@@ -2,10 +2,6 @@ public class Programm
 {
     public static void main(String[] args)
     {
-        System.out.println("test repository");
-        System.out.println("test");
-        System.out.println("я скачал atomic heart");
-        System.out.print("hi world312");
 
     }
 }
